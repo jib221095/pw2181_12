5fa221044ca62fb4398e7f3091fa6313ee97dfe7
jib221095 <jaime_basurto@outlook.com>
2018-04-26 12:55:32 -0600

    termino marvel

--- marvel/js/index.js
+++ marvel/js/index.js
@@ -3,20 +3,41 @@ var marvel = function(){
 		var personaje = $("#personaje").val();
 		var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=";
         var cantidadComics = 0;
+        var catidadSeries = 0; 
         var comics = ""
+        var series = ""
         url = url+personaje;
         $.ajax({dataType:"json",
                  url: url, 
                  success: function(response){
                  	if(response.code == 200){
                  		$("#foto").attr("src",response.data.results[0].thumbnail.path+"."+response.data.results[0].thumbnail.extension);
+                        $("#nombre").html(response.data.results[0].name);
+                        $("#series").html(response.data.results[0].series.items[0].name);
+                        cantidadComics=response.data.results[0].comics.returned;
+                        for(var i=0; i<cantidadComics; i++){
+                            comics+=response.data.results[0].comics.items[i].name+"<br>";
+                            series+=response.data.results[0].series.items[i].name+"<br>";
+                        }
+                        $("#comics").html("Comics: <br>"+comics);
+                        $("#series").html("series: <br>"+series);
                  		
                  		} 
+
+                        
+
 
 	}
 });
     }
-	$("#btnBuscar").on("click",Buscar) /* #por que es un id, on=prende el click  y se dispara el buscar*/
-}
 
+ var teclaPersonaje = function(tecla) {
+    if(tecla.which == 13){
+        Buscar();
+    }
+ }
+ $("#btnBuscar").on("click",Buscar);
+ $("#personaje").on("keypress",teclaPersonaje);
+
+}
 $(document).ready(marvel);
