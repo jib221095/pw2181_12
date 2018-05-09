6c0b9569af5311d238d0507ccbeff1c7042aa234
jib221095 <jaime_basurto@outlook.com>
2018-05-09 13:06:56 -0600

    listado de usuarios

---
+++ listadousuarios/main.js
@@ -0,0 +1,17 @@
+const {app,BrowserWindows} = require('electron')
+const path = require('path')
+const url = require('url')
+let pantallaPrincipal;
+
+function muestraPantallaPrincipal(){
+	pantallaPrincipal=new BrowserWindows({width:320,height:425});
+	pantallaPrincipal.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file',
+		slashes: true
+
+	}))
+	//pantallaPrincipal.webContent.openDevTools();
+	pantallaPrincipal.show();
+}
+app.on('ready',muestraPantallaPrincipal)
