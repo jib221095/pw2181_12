a473cbe33bed8c0c8c45a5735ae38a592340a9f0
jib221095 <jaime_basurto@outlook.com>
2018-05-11 12:56:51 -0600

    pase de lista

--- listadousuarios/index.js
+++ listadousuarios/index.js
@@ -1,27 +1,59 @@
 const $ = require('jquery');
+const {BrowserWindow}=require('electron').remote
+const app = require('electron').app
+const path = require('path')
+const url = require('url')
 
-const {BrowserWindows}=require('electron').remote
-const app=require('electron').app
-const path= require('path')
-const url= require('url')
+let pantallaDetalle
+var usuarios = new Array(20);
 
-let pantallaDetalle;
+function datos(nombre,genero,foto,direccion,telefono){
+	this.nombre = nombre;
+	this.genero = genero;
+	this.foto = foto;
+	this.direccion = direccion;
+	this.telefono = telefono;
+}	
+
 function inicia(){
-	var resultado = "";
-	var nombre ="";
+	var resultado ="";
+	var nombre = "";
 	var foto = "";
-	$.ajax({
-		url: 'https://randomuser.me/api/?results=20',
-		dataType: 'json',
-		seccess: function(data){
-			for(var i=0;i<20;i++){
-				nombre=data.results[i].name.first+" "+data.results[i].name.last;
-				foto=data.results[i].picture.medium;
-				resultado="<li><img src="+foto+">"+nombre+"<button id="+i+">Detalle</button>";
-				$("#lstUsuarios").append(resultado);
-			}
-		}
-	})
+	var genero = "";
+	var direccion = "";
+	var telefono = "";
+	$.ajax({url:'https://randomuser.me/api/?results=20',
+			dataType: 'json',
+			success: function(data){
+				for (var i = 0; i <20; i++) {
+					nombre=data.results[i].name.first+" "+data.results[i].name.last;
+					foto=data.results[i].picture.medium;
+					genero=data.results[i].picture.gender;
+					direccion=data.results[i].picture.street;
+					telefono=data.results[i].picture.phone;
+					resultado="<li><img src="+foto+">"+nombre+"<button id="+i+">Detalle</button>";
+					$("#lstUsuarios").append(resultado);
+					usuarios[i] = new datos(nombre,genero,foto,direccion,telefono);
+				}
+			}})
 }
-
+function botonDetalle(){
+	//alert(this.id);
+	//alert(usuarios[this.id.nombre]);
+	require('electron').remote.getGlobal('infoUsuarios').nombre=usuarios[this.id].nombre;
+	require('electron').remote.getGlobal('infoUsuarios').genero=usuarios[this.id].genero;
+	require('electron').remote.getGlobal('infoUsuarios').foto=usuarios[this.id].foto;
+	require('electron').remote.getGlobal('infoUsuarios').direccion=usuarios[this.id].direccion;
+	require('electron').remote.getGlobal('infoUsuarios').telefono=usuarios[this.id].telefono;
+	
+	pantallaDetalle = new BrowserWindow({width:320,height:425});
+	pantallaDetalle.loadURL(url.format({
+										pathname: path.join(__dirname,'detalleUsuarios.html'),
+										protocol: 'file',
+										slashes: true
+										}));
+	//pantallaDetalle.webContents.openDevTools();
+	pantallaDetalle.show();
+}
+$("body").on("click","li > button",botonDetalle);
 inicia();
--- listadousuarios/main.js
+++ listadousuarios/main.js
@@ -1,16 +1,49 @@
-const {app,BrowserWindows} = require('electron')
+const {app, BrowserWindow} = require('electron') 
 const path = require('path')
 const url = require('url')
+//Constantes para PDF
+const electron = require('electron');
+//sistema de archivos
+const fs = require('fs');
+//acceso al sistema operativo
+const os = require('os');
+//para declarar una funcion remota
+const ipc = electron.ipcMain;
+//acceso a la terminal-linea de comandos
+const shell = electron.shell;
+
 let pantallaPrincipal;
+ 
+ //Objeto global para compartir datos dentre pantallas
+ global.infoUsuarios = {
+	 nombre: '',
+	 genero: '',
+	 foto: '',
+	 direccion: '',
+	 telefono: ''
+
+}
+
+ipc.on('print-to-pdf',function(event){
+	const pdfPath=path.join(os.tmpdir(),'print.pdf')
+	const win=BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+		fs.writeFile(pdfPath,data,function(error){
+			if(error){
+				throw error
+			}
+			shell.openExternal('file://'+pdfPath)
+			win.close()
+		})
+	})
+})
 
 function muestraPantallaPrincipal(){
-	pantallaPrincipal=new BrowserWindows({width:320,height:425});
-	pantallaPrincipal.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
-		protocol: 'file',
-		slashes: true
-
-	}))
+	pantallaPrincipal = new BrowserWindow({width:320,height:425});
+	pantallaPrincipal.loadURL(url.format({pathname: path.join(__dirname,'index.html'),
+										  protocol: 'file',
+										  slashes: true}))
 	//pantallaPrincipal.webContent.openDevTools();
 	pantallaPrincipal.show();
 }
